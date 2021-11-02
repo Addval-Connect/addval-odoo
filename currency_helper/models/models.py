b0ea265db1e3be10ff7a75b2a9ddf8b79a6f06ff
JT Guzman <jtguzman@finaldevstudio.com>
2021-11-01 22:48:02 -0300

    Dory 5

--- currency_helper/models/models.py
+++ currency_helper/models/models.py
@@ -6,5 +6,5 @@ class ProductTemplate(models.Model):
     _name = 'product.template'
     _inherit = 'product.template'
 
-    #currency_indexed = fields.Selection([('usd', 'USD'), ('uf', 'UF'), ('clp', 'CLP')], string='Currency', default='uf')
-    price_indexed = fields.Float()
+    currency_indexed = fields.Selection([('usd', 'USD'), ('uf', 'UF'), ('clp', 'CLP')], string='Moneda Indexada', default='uf')
+    price_indexed = fields.Float(string='Precio Indexado')
