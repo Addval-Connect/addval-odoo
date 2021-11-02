ce0355711924719b9d72d6a6e7e804e54e5bdd6a
JT Guzman <jtguzman@finaldevstudio.com>
2021-11-01 22:23:12 -0300

    Step by step

--- currency_helper/models/models.py
+++ currency_helper/models/models.py
@@ -5,5 +5,5 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    currency_indexed = fields.Selection([('usd', 'USD'), ('uf', 'UF'), ('clp', 'CLP')], string='Currency', default='uf')
+    #currency_indexed = fields.Selection([('usd', 'USD'), ('uf', 'UF'), ('clp', 'CLP')], string='Currency', default='uf')
     price_indexed = fields.Float()
