693481e78e84cbd1cd417d59e6d16f342dcfd7f0
JT Guzman <jtguzman@finaldevstudio.com>
2021-11-01 22:27:04 -0300

    Dory 1

--- currency_helper/models/models.py
+++ currency_helper/models/models.py
@@ -3,6 +3,7 @@
 from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
+    _name = 'product.template'
     _inherit = 'product.template'
 
     #currency_indexed = fields.Selection([('usd', 'USD'), ('uf', 'UF'), ('clp', 'CLP')], string='Currency', default='uf')
