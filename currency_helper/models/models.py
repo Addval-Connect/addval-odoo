2374694999cf9237e5f7e5ca9f3bb86e9f09a824
JTG <jtguzman@addval.cl>
2021-11-06 19:26:13 -0300

    Fix action

--- currency_helper/models/models.py
+++ currency_helper/models/models.py
@@ -10,7 +10,7 @@ class ProductTemplate(models.Model):
     price_indexed = fields.Float(string='Precio Indexado')
 
     # This action calculates list_price with the currency_indexed and price_indexed
-    def compute_price(self, price, currency):
+    def compute_price(self):
         for record in self:
             list_price = price_indexed / currency_indexed.rate
         return True
