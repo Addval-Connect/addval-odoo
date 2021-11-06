30dcbe705b62bd18efaa212430ac465d85681b7f
JTG <jtguzman@addval.cl>
2021-11-06 18:44:13 -0300

    Currency model update

--- currency_helper/models/models.py
+++ currency_helper/models/models.py
@@ -6,5 +6,6 @@ class ProductTemplate(models.Model):
     _name = 'product.template'
     _inherit = 'product.template'
 
-    currency_indexed = fields.Selection([('usd', 'USD'), ('uf', 'UF'), ('clp', 'CLP')], string='Moneda Indexada', default='uf')
+    #currency_indexed = fields.Selection([('usd', 'USD'), ('uf', 'UF'), ('clp', 'CLP')], string='Moneda Indexada', default='uf')
+    currency_indexed = fields.Many2one('res.currency', string='Moneda Indexada', default=lambda self: self.env.user.company_id.currency_id)
     price_indexed = fields.Float(string='Precio Indexado')
