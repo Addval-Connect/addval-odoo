59d7c15fa9e55a140029e1e279593b83e5474a2c
JTG <jtguzman@addval.cl>
2021-11-06 19:21:43 -0300

    Compute price action

--- currency_helper/models/models.py
+++ currency_helper/models/models.py
@@ -6,6 +6,11 @@ class ProductTemplate(models.Model):
     _name = 'product.template'
     _inherit = 'product.template'
 
-    #currency_indexed = fields.Selection([('usd', 'USD'), ('uf', 'UF'), ('clp', 'CLP')], string='Moneda Indexada', default='uf')
     currency_indexed = fields.Many2one('res.currency', string='Moneda Indexada', default=lambda self: self.env.user.company_id.currency_id)
     price_indexed = fields.Float(string='Precio Indexado')
+
+    # This action calculates list_price with the currency_indexed and price_indexed
+    def compute_price(self, price, currency):
+        for record in self:
+            list_price = price_indexed / currency_indexed.rate
+        return True
