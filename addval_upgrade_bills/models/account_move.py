02b631821443e811ccdd096e944a7fc959f6b603
Mitul Shah <mish@odoo.com>
2022-05-03 16:59:46 +0530

    [IMP] addval_upgrade_bills: code improved

--- addval_upgrade_bills/models/account_move.py
+++ addval_upgrade_bills/models/account_move.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.exceptions import ValidationError
 from odoo import models, fields, api, _
-from odoo.osv import expression
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
     def _get_name_invoice_report(self):
-        print("\n custom def _get_name_invoice_report====", self)
         self.ensure_one()
         if self.move_type == 'in_invoice':
-            print("\n if self.move_type == 'in_invoice'====", self.move_type)
             return 'addval_upgrade_bills.report_invoice_document'
         return super()._get_name_invoice_report()
+
